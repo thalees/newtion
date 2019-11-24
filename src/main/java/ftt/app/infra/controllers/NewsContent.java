e0a9a89c52d4501bc38e682551dd9479c1505f86
Thales <thalespereiradejesus@gmail.com>
2019-11-24 16:41:46 -0300

    Added method to show the content of news

--- src/main/java/ftt/app/infra/controllers/NewsContent.java
+++ src/main/java/ftt/app/infra/controllers/NewsContent.java
@@ -1,28 +1,42 @@
 package ftt.app.infra.controllers;
 
+import com.google.gson.Gson;
 import ftt.app.Main;
+import ftt.app.domain.model.DataResponse;
+import ftt.app.domain.model.News;
+import ftt.app.infra.repositories.NewsRepository;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import java.net.URL;
+import java.util.Collection;
 import java.util.ResourceBundle;
 
 @Controller
 public class NewsContent {
 
+    @Autowired
+    private NewsRepository newsRepository;
+
     @FXML
     private ResourceBundle resources;
     
     @FXML
-    private URL location;
+    private Label txtTitle;
 
     @FXML
-    private Button btnReturn;
+    private Label txtContent;
+
+    @FXML
+    private Pane BannerImage;
 
     @FXML
     private Button btnFavNew;
@@ -72,4 +86,22 @@ public class NewsContent {
         assert btnMenu != null : "fx:id=\"btnMenu\" was not injected: check your FXML file 'NewsContent.fxml'.";
     }
 
+    public void setNewsToRead(JSONObject data, int index){
+        Gson gson = new Gson();
+        DataResponse response = gson.fromJson(data.toString(), DataResponse.class);
+        Collection<News> newsCollection = response.getArticles();
+
+        News[] newsArray = newsCollection.toArray(new News[newsCollection.size()]);
+
+        newsRepository.saveAndFlush(newsArray[index]);
+
+        txtTitle.setText(newsArray[index].getTitle());
+        txtContent.setText(newsArray[index].getContent());
+
+        BannerImage.setStyle(
+                "-fx-background-image: url('" + newsArray[index].getUrlToImage() + "'); " +
+                        "-fx-background-position: center center; " +
+                        "-fx-background-repeat: stretch;"
+        );
+    }
 }
